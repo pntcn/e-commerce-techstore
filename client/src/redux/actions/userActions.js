7e790b5cc5ec58f7d3b9b7588e64dbdb191367e1
Nemanja Pantic <pantic2022@gmail.com>
2023-03-17 17:43:50 +0100

    Profile Screen and Cart Icon

--- client/src/redux/actions/userActions.js
+++ client/src/redux/actions/userActions.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { setLoading, setError, userLogin, userLogout } from '../slices/user';
+import { setLoading, setError, userLogin, userLogout, updateUserProfile, resetUpdate } from '../slices/user';
 
 export const login = (email, password) => async (dispatch) => {
   dispatch(setLoading(true));
@@ -9,7 +9,6 @@ export const login = (email, password) => async (dispatch) => {
         'Content-Type': 'application/json',
       },
     };
-
     const { data } = await axios.post('/api/users/login', { email, password }, config);
     dispatch(userLogin(data));
     localStorage.setItem('userInfo', JSON.stringify(data));
@@ -30,7 +29,6 @@ export const logout = () => (dispatch) => {
   localStorage.removeItem('userInfo');
   dispatch(userLogout());
 };
-
 export const register = (name, email, password) => async (dispatch) => {
   dispatch(setLoading(true));
   try {
@@ -39,7 +37,6 @@ export const register = (name, email, password) => async (dispatch) => {
         'Content-Type': 'application/json',
       },
     };
-
     const { data } = await axios.post('/api/users/register', { name, email, password }, config);
     dispatch(userLogin(data));
     localStorage.setItem('userInfo', JSON.stringify(data));
@@ -54,4 +51,36 @@ export const register = (name, email, password) => async (dispatch) => {
       )
     );
   }
+};
+
+export const updateProfile = (id, name, email, password) => async (dispatch, getState) => {
+  const {
+    user: { userInfo },
+  } = getState();
+
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${userInfo.token}`,
+        'Content-Type': 'application/json',
+      },
+    };
+    const { data } = await axios.put(`/api/users/profile/${id}`, { _id: id, name, email, password }, config);
+    localStorage.setItem('userInfo', JSON.stringify(data));
+    dispatch(updateUserProfile(data));
+  } catch (error) {
+    dispatch(
+      setError(
+        error.response && error.response.data.message
+          ? error.response.data.message
+          : error.message
+          ? error.message
+          : 'An unexpected error has occured. Please try again later.'
+      )
+    );
+  }
+};
+
+export const resetUpdateSuccess = () => async (dispatch) => {
+  dispatch(resetUpdate());
 };
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import User from '../models/User.js';
 import asyncHandler from 'express-async-handler';
 import jwt from 'jsonwebtoken';
+import protectRoute from '../middleware/authMiddleware.js';
 
 const userRoutes = express.Router();
 
@@ -9,11 +10,9 @@ const userRoutes = express.Router();
 const genToken = (id) => {
   return jwt.sign({ id }, process.env.TOKEN_SECRET, { expiresIn: '60d' });
 };
-
 const loginUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email });
-
   if (user && (await user.matchPasswords(password))) {
     res.json({
       _id: user._id,
@@ -21,29 +20,26 @@ const loginUser = asyncHandler(async (req, res) => {
       email: user.email,
       isAdmin: user.isAdmin,
       token: genToken(user._id),
+      createdAt: user.createdAt,
     });
   } else {
     res.status(401);
     throw new Error('Invalid email or password.');
   }
 });
-
 // POST register user
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
-
   const userExists = await User.findOne({ email });
   if (userExists) {
     res.status(400);
     throw new Error('We already have an account with that email address.');
   }
-
   const user = await User.create({
     name,
     email,
     password,
   });
-
   if (user) {
     res.status(201).json({
       _id: user._id,
@@ -58,7 +54,33 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 });
 
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: genToken(updatedUser._id),
+      createdAt: updatedUser.createdAt,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found.');
+  }
+});
+
 userRoutes.route('/login').post(loginUser);
 userRoutes.route('/register').post(registerUser);
-
+userRoutes.route('/profile/:id').put(protectRoute, updateUserProfile);
 export default userRoutes;
